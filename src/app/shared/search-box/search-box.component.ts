fce59d4cd9007bff07e2a5f94920b11d60bf0b9e
Franco <fraffa1990@gmail.com>
2024-07-23 19:45:18 -0300

    se implementó un debounce en el search box para que a los 500 ms capture los values y realice la busqueda en by-capital

--- src/app/shared/search-box/search-box.component.ts
+++ src/app/shared/search-box/search-box.component.ts
@@ -1,19 +1,45 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { debounceTime, Subject } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styleUrls: []
 })
-export class SearchBoxComponent {
-
+export class SearchBoxComponent implements OnInit {
+  
+  
+  private debouncer: Subject<string> = new Subject<string>();
+  //Subject: tipo especial de observable
+  
   @Input()
   public placeholder: string ='';
-
+  
   @Output()
   public onValue = new EventEmitter<string>();
+
+  @Output()
+  public onDebounce = new EventEmitter<string>();
+  
+  ngOnInit(): void {
+    this.debouncer
+    .pipe(
+      debounceTime(500)
+    )
+
+    .subscribe( (value) => {
+      this.onDebounce.emit( value );
+      
+    } )
+  }
 
   emitVaule( value:string ): void {
     this.onValue.emit(value);
   }
+
+  onKeyPress( searchTerm: string ) {
+    this.debouncer.next( searchTerm );
+    
+  }
+
 }
