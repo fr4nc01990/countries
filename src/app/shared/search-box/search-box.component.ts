7686427a620ce39766ab497cc8481cb7f104f1d1
Franco <fraffa1990@gmail.com>
2024-07-25 18:48:11 -0300

    Limpieza de suscripciones: se implemento ngOnDestroy para limpiar la memoria de search-box (particularmente la suscripción al debouncer)

--- src/app/shared/search-box/search-box.component.ts
+++ src/app/shared/search-box/search-box.component.ts
@@ -1,38 +1,45 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { debounceTime, Subject } from 'rxjs';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { debounceTime, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styleUrls: []
 })
-export class SearchBoxComponent implements OnInit {
+export class SearchBoxComponent implements OnInit, OnDestroy {
   
   
   private debouncer: Subject<string> = new Subject<string>();
   //Subject: tipo especial de observable
+  private debouncerSuscription?: Subscription;
+
   
   @Input()
   public placeholder: string ='';
   
   @Output()
   public onValue = new EventEmitter<string>();
-
+  
   @Output()
   public onDebounce = new EventEmitter<string>();
   
   ngOnInit(): void {
-    this.debouncer
+    this.debouncerSuscription = this.debouncer
     .pipe(
       debounceTime(500)
     )
-
     .subscribe( (value) => {
       this.onDebounce.emit( value );
       
     } )
   }
-
+  
+  ngOnDestroy(): void {//esto se implementara cada vez q se haya implementado una suscripción en un ngOnInit o se hayan implementado en cualquier parte del componente y este escuchando los cambios y las emisiones
+  this.debouncerSuscription?.unsubscribe();
+  ;
+}
+  
+  
   emitVaule( value:string ): void {
     this.onValue.emit(value);
   }
